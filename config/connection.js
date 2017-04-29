0bdb7d51a5a4364d508e910191bf13539393c423
zachary smith <zachary smith>
2017-04-28 20:15:13 -0500

    added connection

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,33 @@
 
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  port: 3306,
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection = mysql.createConnection({
+  
   host: "localhost",
   user: "root",
   password: "Qwert411!",
   database: "burgers_db"
-});
+  });
+
+};
+
+
+
+
+
+
+// var connection = mysql.createConnection({
+//   port: 3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "Qwert411!",
+//   database: "burgers_db"
+// });
 
 // Make connection.
 connection.connect(function(err) {
